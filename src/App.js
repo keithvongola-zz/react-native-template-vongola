fdfb3cad7b53b3ef7886b4fc0efa0d5fb06613fe
Keith Leung <keithleung@ps.asl.com.hk>
2018-12-19 16:26:23 +0800

    * bug fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { fonts, colors } from '../styles';
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import HomePage from './pages/Home';
 
-export default () => (
-  <View style={styles.container}>
-    <Text style={styles.text}>
-        Home Page
-    </Text>
-  </View>
-);
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <HomePage />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'blue',
-  },
-  text: {
-    fontSize: fonts.big,
-    fontWeight: fonts.bold,
-    colors: colors.white,
   },
 });
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { fonts } from '../styles';
+import { fonts, colors } from '../styles';
 
 export default () => (
-    <View style={styles.container}>
-        <Text style={styles.text}>
-            Home Page
-        </Text>
-    </View>
+  <View style={styles.container}>
+    <Text style={styles.text}>
+        Home Page
+    </Text>
+  </View>
 );
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text : {
-        fontSize: fonts.big,
-    },
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue',
+  },
+  text: {
+    fontSize: fonts.big,
+    fontWeight: fonts.bold,
+    color: colors.white,
+  },
+});
